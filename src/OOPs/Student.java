4d9f1b8b363f3dade3e8c28478cfe7f33a753c62
mini-360 <rajabhay56999@gmail.com>
2025-02-16 16:29:39 +0530

    OOps theory

--- src/OOPs/Student.java
+++ src/OOPs/Student.java
@@ -2,14 +2,29 @@ package OOPs;
 
 public class Student {
     String name;
-    private int rno;
+    int rno;
     double percent;
+    final String schoolName="SVM";
+    private static int numberOfStudents;
 
+    //Constructor
+    public Student(){
+
+    }
+    public  Student(String name,int rno,double percent){
+        this.name=name;
+        this.rno=rno;
+        this.percent=percent;
+        numberOfStudents++;
+    }
     public int getRno() { // getter
         return rno;
     }
 
     public void setRno(int rno) {
         this.rno = rno;
+    }
+    public static int getNumberOfStudents(){
+        return numberOfStudents;
     }
 }
--- src/OOPs/StudentClass.java
+++ src/OOPs/StudentClass.java
@@ -2,15 +2,12 @@ package OOPs;
 
 public class StudentClass {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.name = "Abhay";
-        s1.percent = 75.2;
-        s1.setRno(14);
-        System.out.println(s1.getRno());
-    }
+        Student s1 = new Student("Abhay Raj",21,75.2);
+        System.out.println(Student.getNumberOfStudents());
+        Student s2=new Student("Rohit",45,75);
+        System.out.println(Student.getNumberOfStudents());
 
-    public static void fun(Student x) {
-        System.out.println(x.name);
+
     }
 
     public static void change(Student x) {
