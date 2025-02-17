7a57719ef4b5838558acb90c2b30f2333ebe2723
mini-360 <rajabhay56999@gmail.com>
2025-02-17 20:03:06 +0530

    linked list basic concepts creation

---
+++ src/LinkedList/DisplayLL.java
@@ -0,0 +1,47 @@
+package LinkedList;
+
+public class DisplayLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(9);
+        Node d = new Node(8);
+        Node e = new Node(16);
+
+        // 5 3 9 8 16
+        a.next = b;// 5->3 9 8 16
+        b.next = c;// 5->3->9 8 16
+        c.next = d;// 5->3->9->8 16
+        d.next = e;// 5->3->9->8->16
+        e.next = null; // 5->3->9->8->16->null
+
+        displayIterative(a);
+        System.out.println();
+        displayRecursively(a);
+    }
+    public static void displayIterative(Node head){
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    public static void displayRecursively(Node head){
+        Node temp=head;
+        if (temp==null){
+            return;
+        }
+        System.out.print(temp.data+" ");
+        displayRecursively(temp.next);
+    }
+
+}
---
+++ src/LinkedList/LLBasics.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class LLBasics {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+//            this.next
+        }
+    }
+
+    // Singly LinkedList
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(9);
+        Node d = new Node(8);
+        Node e = new Node(16);
+
+        // 5 3 9 8 16
+        a.next = b;// 5->3 9 8 16
+        b.next = c;// 5->3->9 8 16
+        c.next = d;// 5->3->9->8 16
+        d.next = e;// 5->3->9->8->16
+        e.next = null; // 5->3->9->8->16->null
+
+    }
+}
